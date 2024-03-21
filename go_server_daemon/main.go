d4eb9be87902a2a83a5b3e86c2f6e884cad5d9e2
Andrii Derkach <andrsj.derkach@gmail.com>
2024-03-21 20:10:25 +0200

    working changes

--- go_server_daemon/main.go
+++ go_server_daemon/main.go
@@ -1,6 +1,7 @@
-package httpserver_go
+package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log"
@@ -9,7 +10,7 @@ import (
 	"os"
 	"strings"
 
-	pb "http_railway_server/railway_grpc"
+	pb "http_railway_server/grpc/protobufs"
 
 	"google.golang.org/grpc"
 )
@@ -27,7 +28,7 @@ func reader(w http.ResponseWriter, r *http.Request) {
 			file, err = os.ReadFile("site" + r.URL.Path)
 			sliced := strings.Split(r.URL.Path, ".")
 			type_ := sliced[len(sliced)-1]
-			w.Header().Set("Content-Type", mime.TypeByExtension(type_)) //Fix mime type
+			w.Header().Set("Content-Type", mime.TypeByExtension(type_)) // Fix mime type
 		}
 		if err != nil {
 
@@ -37,7 +38,7 @@ func reader(w http.ResponseWriter, r *http.Request) {
 				os.Exit(-1)
 			}
 		}
-		io.WriteString(w, string(file))
+		_, _ = io.WriteString(w, string(file))
 
 	case "POST":
 		if err := r.ParseForm(); err != nil {
@@ -46,24 +47,32 @@ func reader(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(os.Stdout, "New post request:%v\n", r.PostForm)
 	}
-
 }
+
 func main() {
 	var opts []grpc.DialOption
 	railway_server_ip := "localhost:50051"
-	conn, err := grpc.Dial(*&railway_server_ip, opts...)
-	if err != nil {
 
+	conn, err := grpc.Dial(railway_server_ip, opts...)
+	if err != nil {
+		log.Fatalln(err)
 	}
 	defer conn.Close()
-	client := pb.NewRouteGuideClient(conn)
-	train, err := client.GetTrain(4)
-	if err == nil {
-		println(train.state)
-	}
-	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("./site/image")))) //Image Fix...'''
-	http.HandleFunc("/", reader)
-	println("SERVER STARTED")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 
+	client := pb.NewRailwayClient(conn)
+
+	// Assuming the existence of a context and correct initialization of a Train object.
+	train, err := client.GetTrain(context.Background(), &pb.Train{Id: 4})
+	if err != nil {
+		log.Fatalf("Could not get train: %v", err)
+	}
+
+	fmt.Println(train)
+
+	http.Handle("/image/", http.StripPrefix("/image/", http.FileServer(http.Dir("./site/image")))) // Image Fix...'''
+	http.HandleFunc("/", reader)
+
+	println("SERVER STARTED")
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
